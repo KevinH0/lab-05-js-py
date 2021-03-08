92fd6114e9dbe477956a4f2160de88634f3789de
Kevin Huang <striderster@Kevins-MacBook-Air.local>
2021-03-08 16:53:32 -0500

    added python madlibs

---
+++ madlibs.py
@@ -0,0 +1,10 @@
+noun = input("GIVE ME A NOUN");
+adjective = input("GIVE ADJECTIVE");
+adverb = input("ADVERB NOWWW");
+verb = input("vErb!!!");
+
+
+print("On a tropical "+noun+", ");
+print("Underneath a "+adjective+", lava moon.");
+print("Hangin' "+adverb+", with the hulu dancers,");
+print(verb+" questions cause they all got the answers");
